34ac1557a24e2825eec49624396336f5068f32f7
Natacha Borges <contatonatachaborges@gmail.com>
2023-07-21 19:53:44 -0300

    Aula1: estudos sobre tipos de variáveis

---
+++ src/aula1/Main.java
@@ -0,0 +1,20 @@
+package aula1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int y = 32;
+        double x = 10.35784;
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.println(y);
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
+        System.out.println("bom dia!");
+        System.out.println("RESULTADO  =" + x + " METROS");
+        System.out.printf("RESULTADO = %.2f metros%n", x);
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n " , nome, idade, renda);
+    }
+}
