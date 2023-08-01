a1eb2144653c01f400fedc796a2f99321adb8540
Natacha Borges <contatonatachaborges@gmail.com>
2023-08-01 10:01:15 -0300

    Exercicio3: Desenvolvimento de um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).

---
+++ src/Exercicio3/Main.java
@@ -0,0 +1,25 @@
+package Exercicio3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int A, B, C, D;
+        int Result;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+
+        Result = (A * B - C * D);
+
+        System.out.println("Diferença = " + Result);
+
+
+
+
+    }
+}
