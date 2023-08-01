997a34f9d7bc384d0194c219827b98406560b068
Natacha Borges <contatonatachaborges@gmail.com>
2023-08-01 10:37:46 -0300

    Exercicio4: Um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.

---
+++ src/Exercicio4/Main.java
@@ -0,0 +1,22 @@
+package Exercicio4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double NúmeroDoFuncionário, HorasTrabalhadas, ValorPorhora;
+        double SalárioDoFuncionário;
+
+        NúmeroDoFuncionário = sc.nextDouble();
+        HorasTrabalhadas = sc.nextDouble();
+        ValorPorhora = sc.nextDouble();
+        SalárioDoFuncionário = HorasTrabalhadas * ValorPorhora;
+
+        System.out.printf("Number = %.0f%n", NúmeroDoFuncionário);
+        System.out.printf("salary = U$ %.2f", SalárioDoFuncionário);
+
+    }
+}
