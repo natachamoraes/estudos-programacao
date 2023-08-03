dada3ad87d874a46b354d2944dd7dc4162e5a669
Natacha Borges <contatonatachaborges@gmail.com>
2023-08-03 02:54:19 -0300

    Exercicio8: Um programa para ler um número inteiro, depois dizer se ele é par ou impar.

---
+++ src/Exercicio8/Main.java
@@ -0,0 +1,20 @@
+package Exercicio8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int x;
+
+        x = sc.nextInt();
+        if (x % 2 == 0) {
+            System.out.println("PAR");
+        }
+        else {
+            System.out.println("IMPAR");
+        }
+        sc.close();
+    }
+}
